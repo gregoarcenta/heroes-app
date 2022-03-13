2af0e973e53349449649d68bf332c40f3decfef0
gregory arcentales <gregoarcenta@gmail.com>
2022-03-13 15:12:10 -0500

    se hacer rework del compoente agregar heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Heroe, Publisher } from '../../interfaces/Heroe';
+import { Heroe } from '../../interfaces/Heroe';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
@@ -16,28 +16,22 @@ export class AgregarComponent implements OnInit {
 
   heroeId: string | null = ''
 
-  heroe: Heroe = {
-    superhero: '',
-    alter_ego: '',
-    characters: '',
-    first_appearance: '',
-    publisher: Publisher.DCComics,
-    alt_img: ''
-  }
+  heroe!: Heroe
 
   heroeForm: FormGroup = this.fb.group({
-    superhero: [this.heroe.superhero],
-    alter_ego: [this.heroe.alter_ego],
-    characters: [this.heroe.characters],
-    first_appearance: [this.heroe.first_appearance],
-    publisher: [Publisher.DCComics],
-    alt_img: [this.heroe.alt_img],
+    superhero: [''],
+    alter_ego: [''],
+    characters: [''],
+    first_appearance: [''],
+    publisher: [''],
+    alt_img: [''],
   })
 
   constructor(
     private fb: FormBuilder,
     private heroeService: HeroesService,
     private route: ActivatedRoute,
+    private router: Router,
     private _snackBar: MatSnackBar
   ) { }
 
@@ -46,13 +40,9 @@ export class AgregarComponent implements OnInit {
     if (this.heroeId) {
       this.heroeService.getHeroe(this.heroeId)
         .subscribe(heroe => {
+          const { id, ...heroeUpdate } = heroe
           this.heroe = heroe
-          this.heroeForm.controls['superhero'].setValue(heroe.superhero)
-          this.heroeForm.controls['alter_ego'].setValue(heroe.alter_ego)
-          this.heroeForm.controls['characters'].setValue(heroe.characters)
-          this.heroeForm.controls['first_appearance'].setValue(heroe.first_appearance)
-          this.heroeForm.controls['publisher'].setValue(heroe.publisher)
-          this.heroeForm.controls['alt_img'].setValue(heroe.alt_img)
+          this.heroeForm.setValue(heroeUpdate)
         })
     }
   }
@@ -66,25 +56,18 @@ export class AgregarComponent implements OnInit {
   guardarHeroe() {
     if (this.heroeForm.invalid) return
 
-    this.heroe = {
-      superhero: this.heroeForm.value.superhero,
-      alter_ego: this.heroeForm.value.alter_ego,
-      characters: this.heroeForm.value.characters,
-      first_appearance: this.heroeForm.value.first_appearance,
-      publisher: this.heroeForm.value.publisher,
-      alt_img: this.heroeForm.value.alt_img
-    }
-
     if (this.heroeId) {
-      this.heroeService.updateHeroe(this.heroeId, this.heroe)
+      this.heroeService.updateHeroe(this.heroeId, this.heroeForm.value)
         .subscribe(resp => {
           this.heroeForm.reset()
           this.mostrarSnackBar('Registro actualizado')
+          this.router.navigate(['/heroes', resp.id])
         })
     } else {
-      this.heroeService.addHeroe(this.heroe).subscribe(resp => {
+      this.heroeService.addHeroe(this.heroeForm.value).subscribe(resp => {
         this.heroeForm.reset()
         this.mostrarSnackBar('Registro guardado')
+        this.router.navigate(['/heroes', resp.id])
       })
     }
 
--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -7,7 +7,7 @@ import { Heroe } from '../interfaces/Heroe';
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe): string {
-    if (!heroe.id && !heroe.alt_img) {
+    if (!heroe) {
       return `assets/no-image.png`
     } else if (heroe.alt_img) {
       return heroe.alt_img
