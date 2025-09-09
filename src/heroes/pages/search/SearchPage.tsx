b22cb19d1556f5c31e0f0bbdfad13e40f5cefb42
Gregory Arcentales <gregoarcenta@gmail.com>
2025-09-08 20:40:57 -0500

    feat: add HeroGrid and HeroGridCard components; refactor HomePage to utilize new components
    - Introduced HeroGrid component to display a grid of hero cards.
    - Created HeroGridCard component to encapsulate the structure and styling of individual hero cards.
    - Updated HomePage to replace inline hero card definitions with the new HeroGrid component.
    - Enhanced Progress component to accept an activeColor prop for customizable indicator colors.
    - Added SearchControls component for improved search and filter functionality in SearchPage.

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,4 +1,6 @@
 import { CustomJumbotrom } from "@/components/custom/CustomJumbotron";
+import { SearchControls } from "./ui/SearchControls";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
@@ -7,6 +9,12 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         description="Descubre, explora y administra super héroes y villanos"
       />
+
+      {/* stats dashoboard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
     </>
   );
 };
